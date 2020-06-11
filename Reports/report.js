9b8acbb388d4a0955f20c10e32c003a191d0de0e
charlesselvakumar <charles.selvakumar@yahoo.com>
2020-06-11 19:53:54 +0530

    adding code to jenkins

--- Reports/report.js
+++ Reports/report.js
@@ -167,4 +167,63 @@ formatter.scenario({
   "keyword": "Scenario Outline",
   "tags": [
     {
-      
+      "line": 5,
+      "name": "@Smoke"
+    }
+  ]
+});
+formatter.step({
+  "line": 7,
+  "name": "User enter invalid \"charles\" and \"testdata\"",
+  "matchedColumns": [
+    0,
+    1
+  ],
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 8,
+  "name": "User click login button",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 9,
+  "name": "User must navigate to invalid credentials page",
+  "keyword": "Then "
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "charles",
+      "offset": 20
+    },
+    {
+      "val": "testdata",
+      "offset": 34
+    }
+  ],
+  "location": "Facebook.user_enter_invalid_and(String,String)"
+});
+formatter.result({
+  "duration": 3592557400,
+  "status": "passed"
+});
+formatter.match({
+  "location": "Facebook.user_click_login_button()"
+});
+formatter.result({
+  "duration": 378643578100,
+  "error_message": "org.openqa.selenium.NoSuchElementException: Cannot locate an element using xpath\u003d//button[@id\u003d\u0027u_0_b\u0027]\nFor documentation on this error, please visit: https://www.seleniumhq.org/exceptions/no_such_element.html\nBuild info: version: \u00273.141.59\u0027, revision: \u0027e82be7d358\u0027, time: \u00272018-11-14T08:17:03\u0027\nSystem info: host: \u0027LAPTOP-NQT1QSKC\u0027, ip: \u0027192.168.0.107\u0027, os.name: \u0027Windows 10\u0027, os.arch: \u0027amd64\u0027, os.version: \u002710.0\u0027, java.version: \u00271.8.0_251\u0027\nDriver info: driver.version: RemoteWebDriver\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.findElement(RemoteWebDriver.java:327)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.findElementByXPath(RemoteWebDriver.java:428)\r\n\tat org.openqa.selenium.By$ByXPath.findElement(By.java:353)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.findElement(RemoteWebDriver.java:315)\r\n\tat com.step.Facebook.user_click_login_button(Facebook.java:32)\r\n\tat ✽.And User click login button(Facebook.feature:8)\r\n",
+  "status": "failed"
+});
+formatter.match({
+  "location": "Facebook.user_must_navigate_to_invalid_credentials_page()"
+});
+formatter.result({
+  "status": "skipped"
+});
+formatter.after({
+  "duration": 763128800,
+  "status": "passed"
+});
+});
