d7aaf60e873aef091ce8a057cd94b905a32a97d4
charlesselvakumar <charles.selvakumar@yahoo.com>
2020-06-11 20:21:24 +0530

    file updated

--- Reports/report.js
+++ Reports/report.js
@@ -69,7 +69,7 @@ formatter.examples({
   "keyword": "Examples"
 });
 formatter.before({
-  "duration": 6457797500,
+  "duration": 6474941900,
   "status": "passed"
 });
 formatter.background({
@@ -126,29 +126,29 @@ formatter.match({
   "location": "Facebook.user_enter_invalid_and(String,String)"
 });
 formatter.result({
-  "duration": 2122496000,
+  "duration": 2392755000,
   "status": "passed"
 });
 formatter.match({
   "location": "Facebook.user_click_login_button()"
 });
 formatter.result({
-  "duration": 238900310100,
-  "error_message": "org.openqa.selenium.NoSuchElementException: Cannot locate an element using xpath\u003d//button[@id\u003d\u0027u_0_b\u0027]\nFor documentation on this error, please visit: https://www.seleniumhq.org/exceptions/no_such_element.html\nBuild info: version: \u00273.141.59\u0027, revision: \u0027e82be7d358\u0027, time: \u00272018-11-14T08:17:03\u0027\nSystem info: host: \u0027LAPTOP-NQT1QSKC\u0027, ip: \u0027192.168.0.107\u0027, os.name: \u0027Windows 10\u0027, os.arch: \u0027amd64\u0027, os.version: \u002710.0\u0027, java.version: \u00271.8.0_251\u0027\nDriver info: driver.version: RemoteWebDriver\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.findElement(RemoteWebDriver.java:327)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.findElementByXPath(RemoteWebDriver.java:428)\r\n\tat org.openqa.selenium.By$ByXPath.findElement(By.java:353)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.findElement(RemoteWebDriver.java:315)\r\n\tat com.step.Facebook.user_click_login_button(Facebook.java:32)\r\n\tat ✽.And User click login button(Facebook.feature:8)\r\n",
-  "status": "failed"
+  "duration": 114303500,
+  "status": "passed"
 });
 formatter.match({
   "location": "Facebook.user_must_navigate_to_invalid_credentials_page()"
 });
 formatter.result({
-  "status": "skipped"
+  "duration": 77200,
+  "status": "passed"
 });
 formatter.after({
-  "duration": 794851900,
+  "duration": 714935100,
   "status": "passed"
 });
 formatter.before({
-  "duration": 5432546500,
+  "duration": 5279051400,
   "status": "passed"
 });
 formatter.background({
@@ -205,25 +205,25 @@ formatter.match({
   "location": "Facebook.user_enter_invalid_and(String,String)"
 });
 formatter.result({
-  "duration": 3592557400,
+  "duration": 1776306600,
   "status": "passed"
 });
 formatter.match({
   "location": "Facebook.user_click_login_button()"
 });
 formatter.result({
-  "duration": 378643578100,
-  "error_message": "org.openqa.selenium.NoSuchElementException: Cannot locate an element using xpath\u003d//button[@id\u003d\u0027u_0_b\u0027]\nFor documentation on this error, please visit: https://www.seleniumhq.org/exceptions/no_such_element.html\nBuild info: version: \u00273.141.59\u0027, revision: \u0027e82be7d358\u0027, time: \u00272018-11-14T08:17:03\u0027\nSystem info: host: \u0027LAPTOP-NQT1QSKC\u0027, ip: \u0027192.168.0.107\u0027, os.name: \u0027Windows 10\u0027, os.arch: \u0027amd64\u0027, os.version: \u002710.0\u0027, java.version: \u00271.8.0_251\u0027\nDriver info: driver.version: RemoteWebDriver\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.findElement(RemoteWebDriver.java:327)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.findElementByXPath(RemoteWebDriver.java:428)\r\n\tat org.openqa.selenium.By$ByXPath.findElement(By.java:353)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.findElement(RemoteWebDriver.java:315)\r\n\tat com.step.Facebook.user_click_login_button(Facebook.java:32)\r\n\tat ✽.And User click login button(Facebook.feature:8)\r\n",
-  "status": "failed"
+  "duration": 108195300,
+  "status": "passed"
 });
 formatter.match({
   "location": "Facebook.user_must_navigate_to_invalid_credentials_page()"
 });
 formatter.result({
-  "status": "skipped"
+  "duration": 46700,
+  "status": "passed"
 });
 formatter.after({
-  "duration": 763128800,
+  "duration": 703685500,
   "status": "passed"
 });
 });
